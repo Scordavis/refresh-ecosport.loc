e52d860f132c414b4fad264eee8a57f1ddaaee91
Scordavis <scordavis@gmail.com>
2019-02-06 19:42:28 +0200

    jquery, maskedinput, modal
    закончить оформление css модалки

--- js/common.js
+++ js/common.js
@@ -9,9 +9,23 @@ $(document).ready(function() {
 	// masked input
 	$(function () {
 	  $.mask.definitions['~'] = "[+-]";
+	  //$("#date").mask("99/99/9999",{completed:function(){alert("completed!");}});
+	  //$("#phone").mask("(999) 999-9999");
+	  //$("#phoneExt").mask("(999) 999-9999? x99999");
 	  $(".masked-input").mask("8(999)-999-99-99", {
 	      placeholder: " "
 	  });
+	  //$("#tin").mask("99-9999999");
+	  //$("#ssn").mask("999-99-9999");
+	  //$("#product").mask("a*-999-a999", { placeholder: " " });
+	  //$("#eyescript").mask("~9.99 ~9.99 999");
+	  //$("#po").mask("PO: aaa-999-***");
+	  //$("#pct").mask("99%");
+	  //$("input").blur(function() {
+	  //    $("#info").html("Unmasked value: " + $(this).mask());
+	  //}).dblclick(function() {
+	  //    $(this).unmask();
+	  //});
 	});
 
 
